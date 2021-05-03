ed799e2804c52d0028a44fb5e6d89afa76c1c948
jirodai <mogmogmogmog0306@gmail.com>
2021-05-03 21:34:37 +0900

    validation設定

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  validates :profile, presense: true
+  validates :occupation, presense: true
+  validates :position, presense: true
+end
