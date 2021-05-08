31fc63e6879a8bcdf20a050cf4e6f83330d236a9
jirodai <mogmogmogmog0306@gmail.com>
2021-05-09 04:27:57 +0900

    first_commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,18 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+<<<<<<< Updated upstream
   validates :profile, presense: true
   validates :occupation, presense: true
   validates :position, presense: true
+=======
+  validates :name, presence: true
+  validates :profile, presence: true
+  validates :occupation, presence: true
+  validates :position, presence: true
+
+  has_many :prototypes
+  has_many :comments
+  
+>>>>>>> Stashed changes
 end
