670fca1808d92c5b55d0e44a8cae81503596864b
jirodai <mogmogmogmog0306@gmail.com>
2021-05-09 04:45:56 +0900

    修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,19 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
-<<<<<<< Updated upstream
   validates :profile, presense: true
   validates :occupation, presense: true
   validates :position, presense: true
-=======
   validates :name, presence: true
-  validates :profile, presence: true
-  validates :occupation, presence: true
-  validates :position, presence: true
-
   has_many :prototypes
   has_many :comments
   
->>>>>>> Stashed changes
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "prototypes#index"
-<<<<<<< Updated upstream
   devise_for :users
-=======
   resources :prototypes, only: [:index, :new, :create, :show, :edit, :update, :destroy] do
     resources :comments, only: :create
   end
   resources :users, only: :show
->>>>>>> Stashed changes
+
 end
